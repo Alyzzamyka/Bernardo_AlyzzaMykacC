ea25844e54a386cd4a886cec3a8c5ef631b62ce9
Alyuu <160110871+Alyzzamyka@users.noreply.github.com>
2025-09-10 17:42:57 +0800

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -43,15 +43,19 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 
+<?php
+
+// Default route -> Movies list agad
 $router->get('/', 'MoviesController::index');
+
+// Movies routes
 $router->get('movies', 'MoviesController::index');
 $router->match('movies/create', 'MoviesController::create', ['GET','POST']);
-$router->get('movies', 'MoviesController::index'); 
-$router->match('movies/create', 'MoviesController::create', ['GET','POST']); 
-$router->match('movies/update/{id}', 'MoviesController::update', ['GET','POST']); 
-$router->get('movies/delete/{id}', 'MoviesController::delete'); 
-$router->get('movies/soft-delete/{id}', 'MoviesController::soft_delete'); 
-$router->get('movies/restore/{id}', 'MoviesController::restore'); 
+$router->match('movies/update/{id}', 'MoviesController::update', ['GET','POST']);
+$router->get('movies/delete/{id}', 'MoviesController::delete');
+$router->get('movies/soft-delete/{id}', 'MoviesController::soft_delete');
+$router->get('movies/restore/{id}', 'MoviesController::restore');
+
 
 
 
